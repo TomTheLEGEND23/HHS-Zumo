cf52dcf3fb3af5c61e99b569a3d09d1537807c5a
SchoolTrainee <24045616@student.hhs.nl>
2025-05-09 11:29:06 +0200

    Update IMU initialization parameters and clean up setup function in HHS-Zumo.ino

--- HHS-Zumo.ino
+++ HHS-Zumo.ino
@@ -1,14 +1,11 @@
 #include "IMU.h"
-IMU imu(-2538, -2939.0);
+IMU imu(-1945, -1408.0);
 
 void setup() {
-  Serial.begin(9600);
   imu.init();
 }
 
 void loop() {
-  //Serial.println(imu.accelZ());
-  Serial.println(imu.compassHeading());
-  delay(500);
+
 
 }
